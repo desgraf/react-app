ea4f82a8ed309e557b0d41699de762c815e2eb4c
RUBIO LAFUENTE <Sergio.Rubio@t-systems.com>
2017-11-03 14:02:09 +0100

    Navbar improvements - responsive

--- src/components/uis-navbar/index.js
+++ src/components/uis-navbar/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 import logo from '../../logo.svg';
-import {Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap'; 
 
 import './navbar.css';
@@ -13,35 +12,38 @@ class UisNavbar extends Component {
         return (
             <Navbar inverse collapseOnSelect data-toggle="affix">
                 <Navbar.Header>
-                    <Navbar.Brand className="navbar-brand">
-                        <a href="/"><img alt="Home" src={logo}/></a>
+                    <Navbar.Brand className="navbar-inverse">
+                        <a href="/"><text/> Home</a>
                     </Navbar.Brand>
+                    <Navbar.Toggle />
                 </Navbar.Header>
-                <Nav>
-                    <LinkContainer className="navbar-inverse" to="/link">
-                        <NavItem eventKey={2}>Link</NavItem>
-                    </LinkContainer>
-                    <LinkContainer className="navbar-inverse" to="/who">
-                        <NavItem eventKey={3}>Who Am I ?</NavItem>
-                    </LinkContainer>
-                    <NavDropdown className="navbar-inverse" eventKey={3} title="Language" id="basic-nav-dropdown">
-                        <LinkContainer to="/logout">
-                            <MenuItem eventKey={3.1}>Spanish</MenuItem>    
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer className="navbar-inverse" to="/link">
+                            <NavItem eventKey={2}>Link</NavItem>
                         </LinkContainer>
-                        <LinkContainer to="/logout">
-                            <MenuItem eventKey={3.2}>English</MenuItem>    
-                        </LinkContainer> 
-                        <LinkContainer to="/logout">
-                            <MenuItem eventKey={3.3}>Chinese</MenuItem>    
-                        </LinkContainer> 
-                    </NavDropdown>
-                    <LinkContainer className="navbar-inverse right-position" to="/login">
-                        <NavItem eventKey={4}>Login</NavItem>
-                    </LinkContainer>
-                    <LinkContainer className="navbar-inverse right-position" to="/register">
-                        <NavItem eventKey={5}>Register</NavItem>
-                    </LinkContainer>
-                </Nav>
+                        <LinkContainer className="navbar-inverse" to="/who">
+                            <NavItem eventKey={3}>Who Am I ?</NavItem>
+                        </LinkContainer>
+                        <NavDropdown className="navbar-inverse" eventKey={3} title="Language" id="basic-nav-dropdown">
+                            <LinkContainer to="/logout">
+                                <MenuItem eventKey={3.1}>Spanish</MenuItem>    
+                            </LinkContainer>
+                            <LinkContainer to="/logout">
+                                <MenuItem eventKey={3.2}>English</MenuItem>    
+                            </LinkContainer> 
+                            <LinkContainer to="/logout">
+                                <MenuItem eventKey={3.3}>Chinese</MenuItem>    
+                            </LinkContainer> 
+                        </NavDropdown>
+                        <LinkContainer className="navbar-inverse right-position" to="/login">
+                            <NavItem eventKey={4}>Login</NavItem>
+                        </LinkContainer>
+                        <LinkContainer className="navbar-inverse right-position" to="/register">
+                            <NavItem eventKey={5}>Register</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         );
     }
--- src/components/uis-whoami/index.js
+++ src/components/uis-whoami/index.js
@@ -7,11 +7,13 @@ class UisWhoAmI extends Component {
         return (
             <div>
                 <h1>Who I Am ?</h1>
-                <p>
+                <span>
                     <img className="img-circle" alt="whoami" src={fotoDni}></img>
                     <h3>Sergio Rubio Lafuente</h3>
                     <h5>Full Stack Developer</h5>
-                </p>
+                    <a href="https://www.linkedin.com/in/sergio-rubio-lafuente-693825a2/" target="_blank"><i id="social-ln" className="fa fa-linkedin-square fa-3x"></i></a>
+                    <a href="https://github.com/desgraf" target="_blank"><i id="social-git" className="fa fa-github-square fa-3x"></i></a>
+                </span>
 
             </div>
         );
